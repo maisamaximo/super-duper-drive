0cb06545673410b157e46578ff3950ec0abb62eb
Maisa Maximo <b.outmaself@gmail.com>
2020-12-13 23:29:01 -0300

    starter project

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/SecurityConfiguration.java
@@ -0,0 +1,7 @@
+package com.udacity.jwdnd.course1.cloudstorage.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+}
